ffbd4561f58e8100082e09700835ec1e9b2dbcfd
yurimlee <yuuthod@we-gokorea.com>
2020-10-08 09:52:50 +0900

    #3.4 TV and Discovery Data -- end

--- screens/Favs.js
+++ screens/Favs.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import { movieApi } from "../api";
 
-export default () => (
-  <View>
-    <Text>Fav</Text>
-  </View>
-);
+export default () => {
+  const [discover, setDiscover] = useState({
+    discover: [],
+    error: null,
+  });
+  const getData = async () => {
+    const [discover, error] = await movieApi.discover();
+    setDiscover({
+      discover,
+      error
+    });
+    console.log(discover.length);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <View>
+      <Text>{discover.length}</Text>
+    </View>
+  );
+};
